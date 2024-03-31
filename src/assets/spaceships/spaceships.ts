54749490a9f3ffbcf88fc013256aaa22493403b1
victor loginov <tkoe88@zoho.com>
2024-03-31 21:53:45 +0200

    dummy data added

---
+++ src/assets/spaceships/spaceships.ts
@@ -0,0 +1,99 @@
+import ship1 from './spaceship_01.jpg';
+import ship2 from './spaceship_02.jpg';
+import ship3 from './spaceship_03.jpg';
+import ship4 from './spaceship_04.jpg';
+import ship5 from './spaceship_05.jpg';
+import ship6 from './spaceship_06.jpg';
+import ship7 from './spaceship_07.jpg';
+import ship8 from './spaceship_08.jpg';
+import ship9 from './spaceship_09.jpg';
+import ship10 from './spaceship_10.jpg';
+import ship11 from './spaceship_11.jpg';
+import ship12 from './spaceship_12.jpg';
+
+export const spaceships = [
+  {
+    id: 'sh2i',
+    name: 'Infinite Speed',
+    image: ship1,
+    desc: 'Multi-lateral scalable software',
+    price: 15674,
+  },
+  {
+    id: '30fk',
+    name: 'Space Bullet',
+    image: ship2,
+    desc: 'Multi-tiered impactful model',
+    price: 19774,
+  },
+  {
+    id: 'jf2o',
+    name: "She's the Fastest",
+    image: ship3,
+    desc: 'Managed even-keeled support',
+    price: 14098,
+  },
+  {
+    id: '4jg2',
+    name: 'SuperNova SKR31',
+    image: ship4,
+    desc: 'User-friendly tangible support',
+    price: 15674,
+  },
+  {
+    id: 'gk30',
+    name: 'THE UN5T0PP4BL3',
+    image: ship5,
+    desc: 'Total dynamic moratorium',
+    price: 21301,
+  },
+  {
+    id: 'mg3l',
+    name: "Can't Possibly Lose",
+    image: ship6,
+    desc: 'Fully-configurable actuating local area network',
+    price: 19646,
+  },
+  {
+    id: 'kf4v',
+    name: 'Through the Black Hole',
+    image: ship7,
+    desc: 'Quality-focused intermediate neural-net',
+    price: 15097,
+  },
+  {
+    id: 'b3l4',
+    name: "Sun's Brother",
+    image: ship8,
+    desc: 'Fully-configurable secondary extranet',
+    price: 13369,
+  },
+  {
+    id: 'l2dd',
+    name: 'TIT4N No. 4',
+    image: ship9,
+    desc: 'Customizable foreground architecture',
+    price: 16024,
+  },
+  {
+    id: '0pvf',
+    name: 'Atol Protected',
+    image: ship10,
+    desc: 'Right-sized fresh-thinking middleware',
+    price: 17032,
+  },
+  {
+    id: '3fk5',
+    name: 'Galactic Viper 3-24N-R',
+    image: ship11,
+    desc: 'Cross-platform dynamic paradigm',
+    price: 18013,
+  },
+  {
+    id: '0f24',
+    name: 'Skyon Leader R2',
+    image: ship12,
+    desc: 'Reverse-engineered empowering migration',
+    price: 19024,
+  },
+];
