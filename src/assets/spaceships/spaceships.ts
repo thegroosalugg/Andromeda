64d96a072a31f7199fa0290f7a3333f943254b2a
victor loginov <tkoe88@zoho.com>
2024-04-01 00:29:24 +0200

    added faker and generated spaceship dummy data with it

--- src/assets/spaceships/spaceships.ts
+++ src/assets/spaceships/spaceships.ts
@@ -12,17 +12,15 @@ import ship10 from './spaceship_10.jpg';
 import ship11 from './spaceship_11.jpg';
 import ship12 from './spaceship_12.jpg';
 
-export const spaceships = [
-  new SpaceShip('Infinite Speed', ship1, 'Multi-lateral scalable software', 15674),
-  new SpaceShip('Space Bullet', ship2, 'Multi-tiered impactful model', 19774),
-  new SpaceShip("She's the Fastest", ship3, 'Managed even-keeled support', 14098),
-  new SpaceShip('SuperNova SKR31', ship4, 'User-friendly tangible support', 15674),
-  new SpaceShip('THE UN5T0PP4BL3', ship5, 'Total dynamic moratorium', 21301),
-  new SpaceShip("Can't Possibly Lose", ship6, 'Fully-configurable actuating local area network', 19646),
-  new SpaceShip('Through the Black Hole', ship7, 'Quality-focused intermediate neural-net', 15097),
-  new SpaceShip("Sun's Brother", ship8, 'Fully-configurable secondary extranet', 13369),
-  new SpaceShip('TIT4N No. 4', ship9, 'Customizable foreground architecture', 16024),
-  new SpaceShip('Atol Protected', ship10, 'Right-sized fresh-thinking middleware', 17032),
-  new SpaceShip('Galactic Viper 3-24N-R', ship11, 'Cross-platform dynamic paradigm', 18013),
-  new SpaceShip('Skyon Leader R2', ship12, 'Reverse-engineered empowering migration', 19024),
-];
+import faker from 'faker';
+
+const images = [ship1, ship2, ship3, ship4, ship5, ship6, ship7, ship8, ship9, ship10, ship11, ship12]
+
+const generateSpaceShip = (image: string) => {
+  const name = faker.animal.cat() + ' ' + faker.vehicle.model();
+  const desc = faker.company.catchPhrase();
+  const price = +(1000 + Math.random() * 10000).toFixed(2);
+  return new SpaceShip(name, image, desc, price);
+};
+
+export const spaceships = images.map(image => generateSpaceShip(image));
