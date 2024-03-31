f4944df32f4237f6e5017a59ee49a7cda39f02a3
victor loginov <tkoe88@zoho.com>
2024-03-31 21:04:01 +0200

    further styling and animations on nav bar

--- src/components/navigation/NavButton.tsx
+++ src/components/navigation/NavButton.tsx
@@ -7,13 +7,24 @@ export default function NavButton({ path, name }: { path: string; name: string }
 
   return (
     <li>
-      {isActive && <motion.div layoutId='tab-ufo' className='ufo' />}
+      {isActive && (
+        <motion.div
+          layoutId='tab-ufo'
+          className='ufo'
+          initial={{ opacity: 0.1, y: 100, scale: 0.8 }}
+          animate={{ opacity: 1, y: 0, scale: 1.4 }}
+          transition={{ duration: 0.8, ease: 'easeOut', delay: 0.2 }}
+        />
+      )}
       <NavLink
-        to={path}
-        >
+        className={({ isActive }) => (isActive ? 'active' : 'idle')}
+        to={path} // 'active' class applies automatically without prop. Prop added to set 'idle'
+      >
         {name}
       </NavLink>
-      {isActive && <motion.div layoutId='tab-indicator' className='active-tab' />}
+      {isActive && (
+        <motion.div layoutId='tab-indicator' className='active-tab' />
+      )}
     </li>
   );
 }
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,9 +5,9 @@ export default function Navigation() {
   return (
       <nav className='nav'>
         <ul>
-          <NavButton path='/' name='Home' />
-          <NavButton path='/store' name='SpaceStore' />
-          <NavButton path='/ships' name='SpaceShips' />
+          <NavButton path='/'        name='Home'       />
+          <NavButton path='/store'   name='SpaceStore' />
+          <NavButton path='/ships'   name='SpaceShips' />
           <NavButton path='/explore' name='OuterSpace' />
         </ul>
       </nav>
