ea1d1ad4cdecd50133fe319e57f58384821ea478
victor loginov <tkoe88@zoho.com>
2024-03-31 16:11:25 +0200

    styled navbar and added some global style

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,16 +1,15 @@
+import './Navigation.css'
 import NavButton from './NavButton';
 
 export default function Navigation() {
   return (
-    <header className=''>
-      <nav>
-        <ul className=''>
+      <nav className='nav'>
+        <ul>
           <NavButton path='/' name='Home' />
-          <NavButton path='/store' name='Space Store' />
+          <NavButton path='/store' name='SpaceStore' />
           <NavButton path='/ships' name='SpaceShips' />
           <NavButton path='/explore' name='OuterSpace' />
         </ul>
       </nav>
-    </header>
   );
 }
