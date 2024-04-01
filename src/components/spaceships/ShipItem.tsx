262bce5c0c7b185dc4a65021627a02a13c9c873f
victor loginov <tkoe88@zoho.com>
2024-04-01 04:20:08 +0200

    fine tuning styling

--- src/components/spaceships/ShipItem.tsx
+++ src/components/spaceships/ShipItem.tsx
@@ -3,12 +3,10 @@ import SpaceShip from '../../models/SpaceShip';
 const ShipItem: React.FC<SpaceShip> = ({ name, image, desc, price }) => {
   return (
     <li>
-      <article>
-        <h4>{name}</h4>
-        <img src={image} alt={name} />
-        <p className='ship-info'>{desc}</p>
-        <p className='price'>${price}</p>
-      </article>
+      <h4>{name}</h4>
+      <img src={image} alt={name} />
+      <p className='ship-info'>{desc}</p>
+      <p className='price'>${price}</p>
     </li>
   );
 };
