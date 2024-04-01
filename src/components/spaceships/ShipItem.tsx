02f5750ab243ace2fcca4d54b18e623e01cff49c
victor loginov <tkoe88@zoho.com>
2024-04-01 05:05:18 +0200

    restructured parent & child spaceship FC so can add separate framer motion

--- src/components/spaceships/ShipItem.tsx
+++ src/components/spaceships/ShipItem.tsx
@@ -1,13 +1,14 @@
 import SpaceShip from '../../models/SpaceShip';
+import { motion } from 'framer-motion';
 
 const ShipItem: React.FC<SpaceShip> = ({ name, image, desc, price }) => {
   return (
-    <li>
+    <motion.article whileHover={{ scale: 1.1 }} transition={{ duration: 0.3 }}>
       <h4>{name}</h4>
       <img src={image} alt={name} />
       <p className='ship-info'>{desc}</p>
       <p className='price'>${price}</p>
-    </li>
+    </motion.article>
   );
 };
 
--- src/components/spaceships/SpaceShips.tsx
+++ src/components/spaceships/SpaceShips.tsx
@@ -1,16 +1,33 @@
-import './SpaceShips.css'
+import './SpaceShips.css';
 import SpaceShip from '../../models/SpaceShip';
 import ShipItem from './ShipItem';
+import { motion } from 'framer-motion';
 
 export default function SpaceShips({ spaceships }: { spaceships: SpaceShip[] }) {
   return (
     <main className='spaceship'>
       <h1>SPACESHIP CITY</h1>
-      <ul>
-        {spaceships.map((ship) => (
-          <ShipItem key={ship.id} {...ship} />
+      <motion.ul
+        variants={{
+          visible: { opacity: 1 },
+          hidden: { opacity: 0 },
+        }}
+        initial='hidden'
+        animate='visible'
+      >
+        {spaceships.map((ship, index) => (
+          <motion.li
+            key={ship.id}
+            variants={{
+              visible: { opacity: 1, scale: 1 },
+              hidden: { opacity: 0, scale: 1.2 },
+            }}
+            transition={{ type: 'tween', duration: 0.5, delay: index * 0.1 }}
+          >
+            <ShipItem {...ship} />
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </main>
   );
 }
