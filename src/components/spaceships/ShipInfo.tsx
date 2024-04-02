789c0be80f2d4834b557ff1c276d9d37f1bb5e31
victor loginov <tkoe88@zoho.com>
2024-04-02 03:24:46 +0200

    some motion added to details page

--- src/components/spaceships/ShipInfo.tsx
+++ src/components/spaceships/ShipInfo.tsx
@@ -1,15 +1,27 @@
 import css from './ShipInfo.module.css';
 import SpaceShip from '../../models/SpaceShip';
+import { motion } from 'framer-motion';
 
 const ShipInfo: React.FC<SpaceShip> = ({ name, image, desc, price }) => {
   return (
     <section className={css.ship}>
-      <img src={image} alt={name} />
-      <div className={css['ship-info']}>
+      <motion.img
+        src={image}
+        alt={name}
+        initial={{ x: 200 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5 }}
+      />
+      <motion.div
+        className={css['ship-info']}
+        initial={{ y: 200 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <h2>{name}</h2>
         <p>{desc}</p>
         <p>{price}</p>
-      </div>
+      </motion.div>
     </section>
   );
 };
