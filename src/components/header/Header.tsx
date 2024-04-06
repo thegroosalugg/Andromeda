572e17c8fc288497286a682b8e63614297596bd9
victor loginov <tkoe88@zoho.com>
2024-04-06 18:05:16 +0200

    updated header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,43 @@
+import { useLocation } from 'react-router-dom';
 import MotionDiv from '../UI/MotionDiv';
 import css from './Header.module.css';
+import { AnimatePresence } from 'framer-motion';
 
-export default function Header({ text }: { text: string }) {
+export default function Header() {
+  const { pathname } = useLocation();
+
+  const text = {
+    '/': 'Welcome to Andromeda',
+    '/store': 'Fashion out of this World',
+    '/ships': 'Beyond Earth: Unveiling our Spaceships',
+    '/explore': 'Explore the Galaxy',
+  }[pathname];
+
   return (
-    <MotionDiv
-      element='h1'
-      className={css.header}
-      variants={{
-        hidden: { x: -500, scaleY: 0 },
-        visible: { x: 0, scaleY: [0, 0, 0, 0.5, 1] },
-      }}
-      transition={{ type: 'easeIn', duration: 0.8 }}
-    >
-      {text.toUpperCase()}
-    </MotionDiv>
+    <AnimatePresence>
+      {text && (
+        <MotionDiv
+          element='header'
+          className={css.header}
+          variants={{
+            hidden: { x: -500, scaleY: 0, height: 0 },
+            visible: { x: 0, scaleY: [0, 0, 0, 0.5, 1], height: 'auto' },
+            exit: { scaleY: 0, height: 0 },
+          }}
+          transition={{ type: 'easeIn', duration: 0.8 }}
+        >
+          <MotionDiv
+            key={pathname}
+            element='h1'
+            variants={{
+              hidden: { scaleY: 0 },
+              visible: { scaleY: [0, 0, 0, 0.5, 1] },
+            }}
+          >
+            {text.toUpperCase()}
+          </MotionDiv>
+        </MotionDiv>
+      )}
+    </AnimatePresence>
   );
 }
