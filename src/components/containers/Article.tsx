ea326b8771aee0bdfb4ae117df3ab1212336f8b8
victor loginov <tkoe88@zoho.com>
2024-04-07 20:16:44 +0200

    updated article styling

--- src/components/containers/Article.tsx
+++ src/components/containers/Article.tsx
@@ -6,7 +6,7 @@ const string = 'No TV and no beer make Homer something something. ';
 export default function Article() {
   return (
     <MotionDiv element='article' className={css.content}>
-      <h3>{string}</h3>
+      <h3>FLY THE COUP</h3>
       <p>{string.repeat(100)}</p>
     </MotionDiv>
   );
