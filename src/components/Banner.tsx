71bd28ba4fd6b6abaee59eec15a58cee49f84d9b
victor loginov <tkoe88@zoho.com>
2024-04-08 16:35:56 +0200

    made section disappear when banner appears. added config for narrow screens

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,17 +4,27 @@ import css from './Banner.module.css';
 
 const Banner: React.FC = () => {
   const { scrollY } = useScroll();
+  const screenWidth = window.innerWidth;
+  let bannerRange = [0, 300, 350, 450, 500, 550];
+  let textRange = [0, 500, 600, 700];
+
+  // increase scroll range if content is wrapped
+  if (screenWidth <= 1180) {
+    bannerRange = [0, 700, 800, 900, 1000, 1100]
+    textRange = [0, 900, 1000, 1100]
+  }
+
   const xBanner = useTransform(
     scrollY,
-    [0, 300, 350, 450, 500, 550],
+    [...bannerRange],
     ['-100vw', '-75vw', '-50vw', '-25vw', '-12vw', 0]
   );
   const yText = useTransform(
     scrollY,
-    [0, 500, 600, 700],
+    [...textRange],
     ['-100%', '-50%', '-25%', '0%']
   );
-  const yOpacity = useTransform(scrollY, [0, 500, 600, 700], [0, 0, 0.7, 1]);
+  const yOpacity = useTransform(scrollY, [...textRange], [0, 0, 0.7, 1]);
 
   return (
     <MotionDiv className={css.banner} style={{ x: xBanner }}>
--- src/components/containers/Section.tsx
+++ src/components/containers/Section.tsx
@@ -1,9 +1,28 @@
+import { useScroll, useTransform } from 'framer-motion';
 import MotionDiv from '../UI/MotionDiv';
 import css from './Section.module.css';
 
-export default function Section({children}: {children: React.ReactNode}) {
+export default function Section({ children }: { children: React.ReactNode }) {
+  const { scrollY } = useScroll();
+  const screenWidth = window.innerWidth;
+
+  let scrollRange = [0, 550, 600, 700];
+  const transformVals = [1, 0.8, 0.5, 0];
+
+  // increase scroll range if content is wrapped
+  if (screenWidth <= 1180) {
+    scrollRange = [0, 800, 900, 1100];
+  }
+
+  const opacity = useTransform(scrollY, [...scrollRange], [...transformVals]);
+  const y = useTransform(scrollY, [...scrollRange], ['0%', '-25%', '-50%', '-100%']);
+
   return (
-    <MotionDiv element='section' className={css.section}>
+    <MotionDiv
+      element='section'
+      className={css.section}
+      style={{ opacity, y }}
+    >
       {children}
     </MotionDiv>
   );
