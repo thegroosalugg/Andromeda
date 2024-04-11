4bb08520b0d151c4b2fae60b62766db8ff389b71
victor loginov <tkoe88@zoho.com>
2024-04-12 00:43:27 +0200

    removed smoketrail. going with different approach

--- src/components/banner/ShipBanner.tsx
+++ src/components/banner/ShipBanner.tsx
@@ -4,34 +4,17 @@ import ship2 from '../../assets/banner/spaceship-transparent-02.png';
 import css from './ShipBanner.module.css';
 
 const ShipBanner: React.FC<{ reverse?: boolean }> = ({ reverse }) => {
-  const x = reverse ? '-' : '';
-
-  // chemtrail size
-  const smoke = Array.from({ length: 20 }).map((_, index) => (
-    <div
-      key={index}
-      className={css.smoke}
-      style={{
-        left: `${Math.random() * 100}px`,
-        top: `${Math.random() * 50}px`,
-        animationDelay: `${Math.random() * 2}s`,
-      }}
-    />
-  ));
-
+  
   return (
-    <div className={css.overflow}>
-      <motion.div
-        className={css.banner}
-        initial={{ x: `${x}100%` }}
-        whileInView={{ x: reverse ? '50vw' : '12vw' }}
-        transition={{ type: 'easeIn', duration: 1.5 }}
-        viewport={{ once: true }}
-      >
-        {reverse && smoke}
-        <img src={reverse ? ship2 : ship1} alt='spaceship' />
-        {!reverse && smoke}
-      </motion.div>
+    <div className={css.banner}>
+      <motion.img
+        src={reverse ? ship2 : ship1}
+        alt='spaceship'
+        initial={{ x: reverse ? '-100%' : '100vw' }}
+        whileInView={{ x: reverse ? '100vw' : '-100%' }}
+        transition={{ type: 'easeIn', duration: 2.5 }}
+        // viewport={{ once: true }}
+      />
     </div>
   );
 };
