d14eaf5b90985cf549ac55999fcd1bdde5e9796b
victor loginov <tkoe88@zoho.com>
2024-04-15 19:12:33 +0200

    animater form components

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Input from './Input';
 import css from './Form.module.css';
 import DateInput from './DateInput';
@@ -5,19 +6,31 @@ import DateInput from './DateInput';
 export default function Form() {
   return (
     <form className={css.form}>
-      <div className={css.column}>
-        <Input label='FIRST NAME' id='name' />
-        <Input label='LAST NAME'  id='surname' />
+      <div className={css.row}>
+        <div className={css.column}>
+          <Input label='FIRST NAME' id='name' x={100} y={100} delay={0.2} />
+          <Input label='LAST NAME' id='surname' x={-100} y={-100} delay={0.4} />
+        </div>
+        <div className={css.column}>
+          <Input label='EMAIL' id='email' type='email' x={-100} y={100} delay={0.6} />
+          <Input label='PHONE' id='tel' type='tel' x={100} y={-100} delay={0.8} />
+        </div>
+        <div className={css.column}>
+          <DateInput label='FROM' id='datefrom' x={0} y={-100} delay={1} />
+          <DateInput label='TILL' id='datetill' x={0} y={100} delay={1.2} />
+        </div>
       </div>
       <div className={css.column}>
-        <Input label='EMAIL' id='email' type='email' />
-        <Input label='PHONE' id='tel'   type='tel' />
+        <Input label='PLANET' id='planet' type='select' x={-100} y={0} delay={1.4} />
+        <motion.button
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1, transition: { delay: 2 } }}
+          whileHover={{ scale: 1.2, color: '#FFA500' }}
+          viewport={{ once: true}}
+        >
+          PROCEED
+        </motion.button>
       </div>
-      <div className={css.column}>
-        <DateInput label='FROM' id='datefrom' />
-        <DateInput label='TILL' id='datetill' />
-      </div>
-      <button>PROCEED</button>
     </form>
   );
 }
--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,14 +1,18 @@
+import { motion } from 'framer-motion';
 import css from './Input.module.css';
 
 interface InputProps {
   label: string;
-  id: string;
+     id: string;
   type?: string;
+      x: number;
+      y: number;
+  delay: number;
 }
 
-const Input: React.FC<InputProps> = ({ id, label, type = 'text', ...props }) => {
+const Input: React.FC<InputProps> = ({ id, label, type = 'text', x, y, delay, ...props }) => {
   return (
-    <input
+    <motion.input
       className={css.input}
       id={id}
       name={id}
@@ -16,6 +20,10 @@ const Input: React.FC<InputProps> = ({ id, label, type = 'text', ...props }) => 
       placeholder={label}
       {...props}
       required
+      initial={{ opacity: 0, y, x }}
+      whileInView={{ opacity: 1, y: 0, x: 0 }}
+      viewport={{ once: true }}
+      transition={{ type: 'easeIn', duration: 0.5, delay }}
     />
   );
 };
