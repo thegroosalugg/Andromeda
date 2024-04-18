8a95c82e2daf01e60d28c52554aec8415e5c7a85
victor loginov <tkoe88@zoho.com>
2024-04-18 14:21:22 +0200

    monster animates from different points in the grass

--- src/components/footer/LandingFooter.tsx
+++ src/components/footer/LandingFooter.tsx
@@ -2,10 +2,14 @@ import css from './LandingFooter.module.css';
 import { motion } from 'framer-motion';
 import UFO from '../spaceships/UFO';
 import monster1 from '../../assets/footer/monster1.png';
+import useRepeatAnimation from '../../hooks/useRepeatAnimation';
+import rand from '../../util/rand';
 // import monster2 from '../../assets/footer/monster2.png';
 
 export default function LandingFooter() {
-
+  const duration = useRepeatAnimation({ initialState: 5, stateUpdateFn: () => rand(4, 7) });
+  const x = rand(-4, 4) * 100
+  const scaleX = rand(1, 2) === 1 ? 1 : -1
 
   return (
     <footer className={css.footer}>
@@ -13,10 +17,11 @@ export default function LandingFooter() {
       <motion.img
         src={monster1}
         alt='monster'
+        key={duration}
         className={css.monster}
-        initial={{ y: 100  }}
-        animate={{ y: [0, 100, 0] }}
-        transition={{ duration: 5, repeat: Infinity}}
+        initial={{ y: 100, x, scaleX }}
+        animate={{ y: [100, 0, 100], x }}
+        transition={{ duration }}
       />
       <div className={css.grass} />
     </footer>
