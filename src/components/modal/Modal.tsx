d28795e58c0dfdbec0156cad427482073a1e58d8
victor loginov <tkoe88@zoho.com>
2024-04-20 03:54:07 +0200

    moved animate presense and modal state inside modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,30 +1,37 @@
 import { createPortal } from 'react-dom';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useDispatch } from 'react-redux';
 import { uiActions } from '@/store/uiSlice';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/types';
 import css from './Modal.module.css';
 
 const Modal = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch();
+  const { modal } = useSelector((state: RootState) => state.ui);
 
   function closeModal() {
     dispatch(uiActions.toggle());
   }
 
   return createPortal(
-    <>
-      <div className={css.backdrop} onClick={closeModal} />
-      <motion.dialog
-        open
-        className={css.modal}
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: 1 }}
-        exit={{ scaleY: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        {children}
-      </motion.dialog>
-    </>,
+    <AnimatePresence>
+      {modal && (
+        <>
+          <div className={css.backdrop} onClick={closeModal} />
+          <motion.dialog
+            open
+            className={css.modal}
+            initial={{ scaleY: 0 }}
+            animate={{ scaleY: 1 }}
+            exit={{ scaleY: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            {children}
+          </motion.dialog>
+        </>
+      )}
+    </AnimatePresence>,
     document.getElementById('modal')!
   );
 };
--- src/pageContent/landingPage/monster/MonsterValley.tsx
+++ src/pageContent/landingPage/monster/MonsterValley.tsx
@@ -1,8 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { AnimatePresence } from 'framer-motion';
-import { useSelector } from 'react-redux';
 import { uiActions } from '@/store/uiSlice';
-import { RootState } from '@/store/types';
 import Modal from '@/components/modal/Modal';
 import Monster from './Monster';
 import css from './MonsterValley.module.css';
@@ -10,15 +7,14 @@ import MotionButton from './MotionButton';
 
 export default function MonsterValley() {
   const dispatch = useDispatch();
-  const { modal } = useSelector((state: RootState) => state.ui);
 
   function openModal() {
     dispatch(uiActions.toggle());
   }
-
+  
   return (
     <div className={css.valley}>
-      <AnimatePresence>{modal && <Modal>HELLO</Modal>}</AnimatePresence>
+       <Modal>HELLO</Modal>
       <Monster />
       <MotionButton text='CREDITS' onClick={openModal} />
       <Monster />
