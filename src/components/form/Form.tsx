f0c29255e6632a6262e4778688f52e17cbf7525b
victor loginov <tkoe88@zoho.com>
2024-04-21 21:21:06 +0200

    updated form animations and layout

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -4,33 +4,26 @@ import css from './Form.module.css';
 import DateInput from './DateInput';
 
 export default function Form() {
+  const variants = { hidden: { opacity: 0, scale: 0.5 }, visible: { opacity: 1, scale: 1 } };
+
   return (
-    <form className={css.form}>
-      <div className={css.row}>
-        <div className={css.column}>
-          <Input label='FIRST NAME' id='name' x={100} y={100} delay={0.2} />
-          <Input label='LAST NAME' id='surname' x={-100} y={-100} delay={0.4} />
-        </div>
-        <div className={css.column}>
-          <Input label='EMAIL' id='email' type='email' x={-100} y={100} delay={0.6} />
-          <Input label='PHONE' id='tel' type='tel' x={100} y={-100} delay={0.8} />
-        </div>
-        <div className={css.column}>
-          <DateInput label='FROM' id='datefrom' x={0} y={-100} delay={1} />
-          <DateInput label='TILL' id='datetill' x={0} y={100} delay={1.2} />
-        </div>
-      </div>
-      <div className={css.column}>
-        <Input label='PLANET' id='planet' type='select' x={-100} y={0} delay={1.4} />
-        <motion.button
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1, transition: { delay: 2 } }}
-          whileHover={{ scale: 1.2, color: '#FFA500' }}
-          viewport={{ once: true}}
-        >
-          PROCEED
-        </motion.button>
-      </div>
-    </form>
+    <motion.form
+      className={css.form}
+      transition={{ staggerChildren: 0.2 }}
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true }}
+    >
+      <Input label='FIRST NAME' id='name' />
+      <Input label='LAST NAME' id='surname' />
+      <Input label='EMAIL' id='email' type='email' />
+      <Input label='PHONE' id='tel' type='tel' />
+      <DateInput label='FROM' id='datefrom' />
+      <DateInput label='TILL' id='datetill' />
+      <Input label='PLANET' id='planet' type='select' />
+      <motion.button variants={variants} whileHover={{ scale: 1.2, color: '#FFA500' }}>
+        PROCEED
+      </motion.button>
+    </motion.form>
   );
 }
