3c441ad71f46be6a888d7317289e602199a7f997
victor loginov <tkoe88@zoho.com>
2024-04-21 23:16:44 +0200

    added form overlay

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -7,23 +7,25 @@ export default function Form() {
   const variants = { hidden: { opacity: 0, scale: 0.5 }, visible: { opacity: 1, scale: 1 } };
 
   return (
-    <motion.form
-      className={css.form}
-      transition={{ staggerChildren: 0.1 }}
-      initial='hidden'
-      whileInView='visible'
-      viewport={{ once: true }}
-    >
-      <Input label='FIRST NAME' id='name' />
-      <Input label='LAST NAME' id='surname' />
-      <Input label='EMAIL' id='email' type='email' />
-      <Input label='PHONE' id='tel' type='tel' />
-      <DateInput label='FROM' id='datefrom' />
-      <DateInput label='TILL' id='datetill' />
-      <Input label='PLANET' id='planet' type='select' />
-      <motion.button variants={variants} whileHover={{ scale: 1.2, color: '#FFA500' }}>
-        PROCEED
-      </motion.button>
-    </motion.form>
+    <div className={css.overlay}>
+      <motion.form
+        className={css.form}
+        transition={{ staggerChildren: 0.1 }}
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+      >
+        <Input label='FIRST NAME' id='name' />
+        <Input label='LAST NAME' id='surname' />
+        <Input label='EMAIL' id='email' type='email' />
+        <Input label='PHONE' id='tel' type='tel' />
+        <DateInput label='FROM' id='datefrom' />
+        <DateInput label='TILL' id='datetill' />
+        <Input label='PLANET' id='planet' type='select' />
+        <motion.button variants={variants} whileHover={{ scale: 1.2, color: '#FFA500' }}>
+          PROCEED
+        </motion.button>
+      </motion.form>
+    </div>
   );
 }
