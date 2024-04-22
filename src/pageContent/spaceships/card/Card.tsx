e3a59e040e25b7f71a629e87b6c33b7e225cb4bf
victor loginov <tkoe88@zoho.com>
2024-04-22 21:45:05 +0200

    update cards components

--- src/pageContent/spaceships/card/Card.tsx
+++ src/pageContent/spaceships/card/Card.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import css from './Card.module.css';
 
-export default function Card({ reverse }: { reverse?: boolean }) {
+export default function Card({ reverse, image }: { reverse?: boolean; image: string }) {
   const x = reverse ? '-100%' : '100%';
 
   return (
@@ -10,6 +10,8 @@ export default function Card({ reverse }: { reverse?: boolean }) {
       variants={{ hidden: { x, opacity: 0 }, visible: { x: 0, opacity: 1 } }}
       transition={{ duration: 1, type: 'tween' }}
     >
+      <p>{"I'm waiting for a bus. ".repeat(20)}</p>
+      <img src={image} alt='rocket' />
     </motion.div>
   );
 }
--- src/pageContent/spaceships/card/Cards.tsx
+++ src/pageContent/spaceships/card/Cards.tsx
@@ -1,22 +1,22 @@
-import rocket0 from '@/assets/rockets/rocket0.jpg'
-import rocket1 from '@/assets/rockets/rocket1.jpg'
-import rocket2 from '@/assets/rockets/rocket2.jpg'
-import rocket3 from '@/assets/rockets/rocket3.jpg'
-import rocket4 from '@/assets/rockets/rocket4.jpg'
-import rocket5 from '@/assets/rockets/rocket5.jpg'
-import rocket6 from '@/assets/rockets/rocket6.jpg'
-import rocket7 from '@/assets/rockets/rocket7.jpg'
-import rocket8 from '@/assets/rockets/rocket8.jpg'
-import rocket9 from '@/assets/rockets/rocket9.jpg'
+import rocket0 from '@/assets/rockets/rocket0.jpg';
+import rocket1 from '@/assets/rockets/rocket1.jpg';
+import rocket2 from '@/assets/rockets/rocket2.jpg';
+import rocket3 from '@/assets/rockets/rocket3.jpg';
+import rocket4 from '@/assets/rockets/rocket4.jpg';
+import rocket5 from '@/assets/rockets/rocket5.jpg';
+import rocket6 from '@/assets/rockets/rocket6.jpg';
+import rocket7 from '@/assets/rockets/rocket7.jpg';
+import rocket8 from '@/assets/rockets/rocket8.jpg';
+import rocket9 from '@/assets/rockets/rocket9.jpg';
 import { motion } from 'framer-motion';
 import Card from './Card';
 import css from './Cards.module.css';
 
+const rockets = [rocket0, rocket1, rocket2, rocket3, rocket4, rocket5, rocket6, rocket7, rocket8, rocket9];
+const images = [...rockets].sort(() => Math.random() - 0.5).slice(0, 4); // select 4 random images
 
 export default function Cards() {
-  const cards = [rocket0, rocket1, rocket2, rocket3, rocket4, rocket5, rocket6, rocket7, rocket8, rocket9]
-
-  const CardPair = () => (
+  const CardPair = ({images}: { images: string[] }) => (
     <motion.div
       className={css.pair}
       initial='hidden'
@@ -24,8 +24,8 @@ export default function Cards() {
       transition={{ staggerChildren: 0.3 }}
       viewport={{ once: true }}
     >
-      <Card />
-      <Card reverse />
+      <Card image={images[0]} />
+      <Card image={images[1]} reverse />
     </motion.div>
   );
 
@@ -36,8 +36,8 @@ export default function Cards() {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <CardPair />
-      <CardPair />
+      <CardPair images={[images[0], images[1]]} />
+      <CardPair  images={[images[2], images[3]]}/>
     </motion.div>
   );
 }
