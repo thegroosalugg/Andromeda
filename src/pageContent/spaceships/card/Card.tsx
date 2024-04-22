59c12c0587401f9c5f73ee90e9a94eaa75fa6160
victor loginov <tkoe88@zoho.com>
2024-04-22 22:30:52 +0200

    updated cards to have unique props on each side

--- src/pageContent/spaceships/card/Card.tsx
+++ src/pageContent/spaceships/card/Card.tsx
@@ -3,15 +3,18 @@ import css from './Card.module.css';
 
 export default function Card({ reverse, image }: { reverse?: boolean; image: string }) {
   const x = reverse ? '-100%' : '100%';
+  const text = <p>{"I'm waiting for a bus. ".repeat(20)}</p>;
+  const classes = `${css.card} ${css[reverse ? 'right' : 'left']}`;
 
   return (
     <motion.div
-      className={css.card}
+      className={classes}
       variants={{ hidden: { x, opacity: 0 }, visible: { x: 0, opacity: 1 } }}
       transition={{ duration: 1, type: 'tween' }}
     >
-      <p>{"I'm waiting for a bus. ".repeat(20)}</p>
+      {reverse && text}
       <img src={image} alt='rocket' />
+      {!reverse && text}
     </motion.div>
   );
 }
