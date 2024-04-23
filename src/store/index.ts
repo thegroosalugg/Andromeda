3f1958cfa54d972d1c9691fbedb03c07d980a419
victor loginov <tkoe88@zoho.com>
2024-04-23 15:10:22 +0200

    renamed UI slice to modal slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import shipsSlice from './shipsSlice';
-import uiSlice from './uiSlice';
+import modalSlice from './modalSlice';
 
-const store = configureStore({ reducer: { ships: shipsSlice, ui: uiSlice } });
+const store = configureStore({ reducer: { ships: shipsSlice, modal: modalSlice } });
 
 export default store;
