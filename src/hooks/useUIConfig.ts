f3bbef6bdd20c97daf980cbdcc863be1b78bdc0b
victor loginov <tkoe88@zoho.com>
2024-04-23 17:23:21 +0200

    updates to UI config hook

--- src/hooks/useUIConfig.ts
+++ src/hooks/useUIConfig.ts
@@ -1,20 +1,20 @@
 import { useLocation } from 'react-router-dom';
 
 interface Config {
-  [pathname: string]: { background: string; text: string };
+  [pathname: string]: { background?: string; text: string };
 }
 // prettier-ignore
 const config: Config = {
-    default:  { background: '#7d7d7d00', text: '' },
-         '/': { background: '#7d7d7d00', text: 'Welcome to Andromeda' },
-    '/store': { background: '#7d7d7d00', text: 'Fashion out of this World' },
-    '/ships': { background: '#232728f9', text: 'Beyond Earth: Unveiling our Spaceships' },
-  '/explore': { background: '#7d7d7d00', text: 'Explore the Galaxy' },
+     default: { text: '', background: '#00000000', },
+         '/': { text: 'Welcome to Andromeda' },
+    '/store': { text: 'Fashion out of this World' },
+    '/ships': { text: 'Beyond Earth: Unveiling our Spaceships', background: '#232728f9' },
+  '/explore': { text: 'Explore the Galaxy' },
 };
 
 const useUIConfig = () => {
   const { pathname } = useLocation();
-  const configuredPath = config[pathname] || config.default
+  const configuredPath = { ...config.default, ...config[pathname] }; // add all default values then overwrite any uniques
 
   console.clear(); console.log('find me in /hooks', configuredPath); // log & clear
 
