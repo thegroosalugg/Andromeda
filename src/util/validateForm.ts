a1863b1d43742bf4f3a53048f11c454403d55f88
victor loginov <tkoe88@zoho.com>
2024-04-24 16:56:57 +0200

    update validation logic

--- src/util/validateForm.ts
+++ src/util/validateForm.ts
@@ -2,14 +2,27 @@ interface FormData {
   name?: string;
   surname?: string;
   email?: string;
-  tel?: number;
-  datefrom?: Date;
-  datetill?: Date;
+  tel?: string;
+  datefrom?: string;
+  datetill?: string;
   destination?: string;
 }
 
 const validateForm = (data: FormData) => {
   const errors: Partial<FormData> = {};
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const telRegex = /^\d+$/;
+
+  Object.keys(data).forEach((key) => {
+    const field = key as keyof FormData;
+    if (!data[field]) {
+      errors[field] = 'Empty field';
+    } else if (key === 'email' && !emailRegex.test(data[key] as string)) {
+      errors[key] = 'Invalid email';
+    } else if (key === 'tel' && !telRegex.test(data[key] as string)) {
+      errors[key] = 'Invalid phone';
+    }
+  });
 
   return errors;
 };
