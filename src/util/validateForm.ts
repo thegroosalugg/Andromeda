4b1676707fe3447fd73953af01e922bb3014780d
victor loginov <tkoe88@zoho.com>
2024-04-30 19:02:06 +0200

    outsourced validatedates to own function. Main function clean

--- src/util/validateForm.ts
+++ src/util/validateForm.ts
@@ -23,9 +23,37 @@ export function validateCredentials(
   const regex = field === 'email' ? emailExp : phoneExp;
 
   if (!regex.test(value!)) {
-    return `${field.toUpperCase()} INVALID`;
+    return `${field} INVALID`.toUpperCase();
   } else if (users.some((user) => user[field as 'email' | 'phone'] === value)) {
-    return `${field.toUpperCase()} EXISTS`;
+    return `${field} EXISTS`.toUpperCase();
+  }
+}
+
+export function validateDates(data: FormData, users: User[], shipId: string) {
+  if (data.from && data.till) {
+    const currentFrom = new Date(data.from);
+    const currentTill = new Date(data.till);
+
+    if (currentFrom > currentTill) {
+      return 'NO TIME TRAVELLING';
+    } else {
+      for (const user of users) {
+        for (const booking of user.bookings) {
+          if (booking.shipId === shipId) {
+            const bookedFrom = new Date(booking.from);
+            const bookedTill = new Date(booking.till);
+
+            if (
+              (currentFrom >= bookedFrom && currentFrom <= bookedTill) ||
+              (currentTill >= bookedFrom && currentTill <= bookedTill) ||
+              (currentFrom <= bookedFrom && currentTill >= bookedTill)
+            ) {
+              return 'DATES UNAVAILABLE';
+            }
+          }
+        }
+      }
+    }
   }
 }
 
@@ -34,37 +62,18 @@ export function validateForm(data: FormData, users: User[], shipId: string) {
 
   Object.keys(data).forEach((key) => {
     const field = key as keyof FormData;
-
     if (!data[field]?.trim()) {
       errors[field] = `${[key]} empty`.toUpperCase();
     } else if (field === 'from' || field === 'till') {
-      if (data.from && data.till) {
-        const currentFrom = new Date(data.from);
-        const currentTill = new Date(data.till);
-
-        if (currentFrom > currentTill) {
-          errors.till = errors.from = 'NO TIME TRAVELLING';
-        } else {
-          users.forEach((user) => {
-            user.bookings.forEach((booking) => {
-              if (booking.shipId === shipId) {
-                const bookedFrom = new Date(booking.from);
-                const bookedTill = new Date(booking.till);
-
-                if (
-                  (currentFrom >= bookedFrom && currentFrom <= bookedTill) ||
-                  (currentTill >= bookedFrom && currentTill <= bookedTill) ||
-                  (currentFrom <= bookedFrom && currentTill >= bookedTill)
-                ) {
-                  errors.from = errors.till = 'DATES UNAVAILABLE';
-                }
-              }
-            });
-          });
-        }
+      const dateError = validateDates(data, users, shipId);
+      if (dateError) {
+        errors[field] = dateError;
       }
     } else if (field === 'email' || field === 'phone') {
-      errors[field] = validateCredentials(field, data, users);
+      const credentialError = validateCredentials(field, data, users);
+      if (credentialError) {
+        errors[field] = credentialError;
+      }
     }
   });
 
