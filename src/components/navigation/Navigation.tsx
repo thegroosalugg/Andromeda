e8a905504118fade9e86677ab4e3f2ba59e4929a
victor loginov <tkoe88@zoho.com>
2024-05-03 17:39:35 +0200

    simplified nav styles. Added some userportal styles

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -7,22 +7,18 @@ export default function Navigation() {
   const { background } = useUIConfig();
 
   return (
-    <>
-      <motion.nav
-        className={css.nav}
-        animate={{
-          background,
-          transition: { duration: 0.8, ease: 'easeInOut' },
-        }}
-      >
-        <ul>
-          <NavButton path='/' name='H0M3' />
-          <NavButton path='/store' name='ST0RE' />
-          <NavButton path='/ships' name='SH1P5' />
-          <NavButton path='/explore' name='EXPL0R3' />
-          <NavButton path='/user' name='P0RTAL' />
-        </ul>
-      </motion.nav>
-    </>
+    <motion.ul
+      className={css.nav}
+      animate={{
+        background,
+        transition: { duration: 0.8, ease: 'easeInOut' },
+      }}
+    >
+      <NavButton path='/' name='H0M3' />
+      <NavButton path='/store' name='ST0RE' />
+      <NavButton path='/ships' name='SH1P5' />
+      <NavButton path='/explore' name='EXPL0R3' />
+      <NavButton path='/user' name='P0RTAL' />
+    </motion.ul>
   );
 }
--- src/pageContent/portal/UserPortal.tsx
+++ src/pageContent/portal/UserPortal.tsx
@@ -1,22 +1,28 @@
 import User from '@/models/User';
 import { logout } from '@/store/userSlice';
 import { useDispatch } from 'react-redux';
+import css from './UserPortal.module.css';
 
 export default function UserPortal(user: User) {
+  const { name, surname, email, phone, bookings } = user;
   const dispatch = useDispatch();
 
-   return (
+  return (
     <>
+      <section className={css.portal}>
+        <div className={css.details}>
+          <h2>{name}</h2>
+          <h2>{surname}</h2>
+          <h2>{email}</h2>
+          <h2>{phone}</h2>
+        </div>
+        <ul>
+          {bookings.map((booking) => (
+            <li key={booking.id}>{booking.shipId}</li>
+          ))}
+        </ul>
+      </section>
       <button onClick={() => dispatch(logout())}>LOGOUT</button>
-      <h2>{user.name}</h2>
-      <h2>{user.surname}</h2>
-      <h2>{user.email}</h2>
-      <h2>{user.phone}</h2>
-      <ul>
-        {user.bookings.map((booking) => (
-          <li key={booking.id}>{booking.shipId}</li>
-        ))}
-      </ul>
     </>
   );
 }
