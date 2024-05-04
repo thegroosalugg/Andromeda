9663bc30d5eca15a0d8ebfc27c7e5d7cdecc7c55
victor loginov <tkoe88@zoho.com>
2024-05-04 12:46:40 +0200

    validating editable inputs in user portal

--- src/pageContent/portal/UserPortal.tsx
+++ src/pageContent/portal/UserPortal.tsx
@@ -1,14 +1,34 @@
 import User from '@/models/User';
 import { logout } from '@/store/userSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import css from './UserPortal.module.css';
 import List from '@/components/list/List';
 import Input from '@/components/form/Input';
 import { FormData } from '@/models/FormData';
+import { validateUser } from '@/util/validateForm';
+import { RootState } from '@/store/types';
+import { setErrors } from '@/store/formSlice';
 
 export default function UserPortal(user: User) {
   const { id, bookings, ...userDetails } = user;
+  const { users } = useSelector((state: RootState) => state.users);
+  const { data } = useSelector((state: RootState) => state.form);
   const dispatch = useDispatch();
+
+  function updateHandler() {
+    const editedData = Object.fromEntries(
+      Object.entries(data).filter(entry => entry[1])
+    );
+
+    const errors = validateUser(editedData, users);
+
+    if (errors) {
+      dispatch(setErrors(errors));
+      console.log('USER PORTAL ERRORS', errors)
+    }
+  }
+
+  console.log('USER PORTAL FORMDATA', data)
 
   return (
     <>
@@ -26,6 +46,7 @@ export default function UserPortal(user: User) {
           {(booking) => <p>{booking.shipId}</p>}
         </List>
       </section>
+      <button onClick={updateHandler}>SAVE</button>
       <button onClick={() => dispatch(logout())}>LOGOUT</button>
     </>
   );
