16386c94df2b2aae636d29cfe236cb07255db649
victor loginov <tkoe88@zoho.com>
2024-05-05 17:19:14 +0200

    added booking styling & content

--- src/pageContent/portal/BookedItem.tsx
+++ src/pageContent/portal/BookedItem.tsx
@@ -1,14 +1,45 @@
 import useSearch from '@/hooks/useSearch';
 import Booking from '@/models/Booking';
+import css from './BookedItem.module.css';
+import formatDate from '@/util/formatDate';
 
 const BookedItem = (booking: Booking) => {
+  const { shipId, from, till, pickup, dropoff } = booking;
   const { item: ship } = useSearch({
-    search: { id: booking.shipId, withParams: false },
+    search: { id: shipId, withParams: false },
     reducer: 'ships',
     sliceKey: 'ships',
   });
 
-  return <img src={ship!.image} alt='ship' />;
+  return (
+    <article className={css.booking}>
+      <img src={ship!.image} alt='ship' />
+      <div className={css.details}>
+        <h6>{ship!.maker}</h6>
+        <h5>{ship!.model}</h5>
+        <div className={css.row}>
+          <div className={css.col}>
+            <p style={{ color: '#bb6412d0'}}>start</p>
+            <p>{formatDate(from)}</p>
+          </div>
+          <div className={css.col}>
+            <p style={{ color: '#bb11a1bf'}}>end</p>
+            <p>{formatDate(till)}</p>
+          </div>
+        </div>
+        <div className={css.row}>
+          <div className={css.col}>
+            <p style={{ color: '#bb6412d0'}}>PickUp</p>
+            <p>{pickup}</p>
+          </div>
+          <div className={css.col}>
+            <p style={{ color: '#bb11a1bf'}}>DropOff</p>
+            <p>{dropoff}</p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default BookedItem;
