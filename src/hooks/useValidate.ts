c2b7284fbd5578e0609de24f03e0ed3a732cff01
victor loginov <tkoe88@zoho.com>
2024-05-05 11:27:01 +0200

    updated validate conditions to prevent empty class generations when not needed

--- src/hooks/useValidate.ts
+++ src/hooks/useValidate.ts
@@ -26,46 +26,38 @@ const useValidate = ({ withBooking, updateId, loggingIn }: ValidateOptions = {})
   const navigate = useNavigate();
 
   return () => {
-    const editedData = updateId
-      ? Object.fromEntries(Object.entries(data).filter((entry) => entry[1]))
-      : {};
+    const editedData = updateId ? Object.fromEntries(Object.entries(data).filter((entry) => entry[1])) : {};
     const updateErrors = updateId ? validateUser(editedData, users) : {};
     const userErrors = !user && !loggingIn ? validateUser({ name, surname, email, phone }, users) : {};
-    const bookingErrors = withBooking
-      ? validateBooking({ from, till, pickup, dropoff }, users, shipId!)
-      : {};
+    const bookingErrors = withBooking ? validateBooking({ from, till, pickup, dropoff }, users, shipId!) : {};
     const loginErrs = loggingIn ? validateLogin(login, users) : {};
-
-    captainsLog(
-      { updateId },
-      { loggingIn },
-      { user },
-      { users },
-      { updateErrors },
-      { userErrors },
-      { bookingErrors },
-      { loginErrs }
-    );
 
     const newErrors = { ...userErrors, ...bookingErrors, ...updateErrors, ...loginErrs };
     dispatch(setErrors(newErrors));
 
     if (Object.keys(newErrors).length === 0) {
-      const currentUser = user ? user : new User(name!, surname!, email!, phone!).toObject!();
-      const booking =
-        withBooking && new Booking(shipId!, from!, till!, pickup!, dropoff!).toObject();
+      let currentUser;
 
-      !user && !loggingIn && dispatch(addUser(currentUser));
-      booking && dispatch(addBooking({ currentUser, booking }));
+      if (user) {
+        currentUser = user;
+      } else if (!loggingIn) {
+        currentUser = new User(name!, surname!, email!, phone!).toObject!();
+        dispatch(addUser(currentUser));
+      }
+
+      const booking = withBooking && new Booking(shipId!, from!, till!, pickup!, dropoff!).toObject();
+
+      booking && currentUser && dispatch(addBooking({ currentUser, booking }));
       updateId && dispatch(updateUser(editedData as User));
       loggingIn && dispatch(setUser({ email: login! }));
       dispatch(clearForm());
 
       if (!updateId) {
         window.scrollTo(0, 125);
-        navigate('/user');
+        !loggingIn && navigate('/user');
       }
     }
+    captainsLog({newErrors}, {user}, {users})
   };
 };
 
