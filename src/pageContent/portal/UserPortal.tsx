5282769020c1ea52acfe998d8e0bad61a725be7b
victor loginov <tkoe88@zoho.com>
2024-05-11 17:30:37 +0200

    added confirmation dialogue for deleting account

--- src/pageContent/portal/UserPortal.tsx
+++ src/pageContent/portal/UserPortal.tsx
@@ -1,6 +1,6 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout, deleteUser, addBooking } from '@/store/userSlice';
+import { logout, deleteUser } from '@/store/userSlice';
 import { clearForm } from '@/store/formSlice';
 import { RootState } from '@/store/types';
 import useValidate from '@/hooks/useValidate';
@@ -9,17 +9,22 @@ import Input from '@/components/form/Input';
 import BookedItem from './BookedItem';
 import Modal from '@/components/modal/Modal';
 import EditBooking from './EditBooking';
-import Booking from '@/models/Booking';
 import User from '@/models/User';
 import { FormData } from '@/models/FormData';
 import css from './UserPortal.module.css';
 import NoBookings from './NoBookings'; // I am also not needed post testing
+import { useState } from 'react';
 
 export default function UserPortal(user: User) {
   const { id, bookings, ...userDetails } = user;
   const { item } = useSelector((state: RootState) => state.modal);
   const validate = useValidate({ update: { userId: id } });
   const dispatch = useDispatch();
+  const [confirmDialog, setConfirmDialog] = useState(false);
+
+  function confirmHandler() {
+    setConfirmDialog((prev) => !prev);
+  }
 
   function handleLogout() {
     window.scrollTo(0, 125);
@@ -59,33 +64,26 @@ export default function UserPortal(user: User) {
       </motion.section>
 
       <div className={css.actions}>
-        <button onClick={handleLogout}>
-          LOGOUT
-        </button>
-        <button onClick={() => dispatch(deleteUser(user.id))}>
-          DELETE ACCOUNT
-        </button>
+        <button onClick={handleLogout}>LOGOUT</button>
+        <AnimatePresence mode='wait'>
+          <motion.div
+            key={'a' + confirmDialog}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 1 }}
+            exit={{ opacity: 0, x: 100 }}
+            className={css.dialog}
+          >
+            {!confirmDialog ? (
+              <button onClick={confirmHandler}>DELETE ACCOUNT</button>
+            ) : (
+              <>
+                <button onClick={() => dispatch(deleteUser(user.id))}>CONFIRM</button>
+                <button onClick={confirmHandler} className={css.cancel}>CANCEL</button>
+              </>
+            )}
+          </motion.div>
+        </AnimatePresence>
       </div>
-
-      {/* DELETE ME AFTER TESTING */}
-      <button
-        onClick={() =>
-          dispatch(
-            addBooking({
-              currentUser: user,
-              booking: new Booking(
-                'SP90532',
-                '2024-05-10T22:49:49.340Z',
-                '2024-05-10T22:49:51.771Z',
-                'Saturn',
-                'Saturn'
-              ).toObject!(),
-            })
-          )
-        }
-      >
-        ADD BOOKING
-      </button>
     </motion.section>
   );
 }
