deddd1a2d74c861ae493ca75a014b198e847b863
victor loginov <tkoe88@zoho.com>
2024-05-11 19:57:13 +0200

    outsources useractions as component

--- src/pageContent/portal/UserPortal.tsx
+++ src/pageContent/portal/UserPortal.tsx
@@ -1,7 +1,5 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout, deleteUser } from '@/store/userSlice';
-import { clearForm } from '@/store/formSlice';
+import { motion } from 'framer-motion';
+import { useSelector } from 'react-redux';
 import { RootState } from '@/store/types';
 import useValidate from '@/hooks/useValidate';
 import List from '@/components/list/List';
@@ -9,28 +7,17 @@ import Input from '@/components/form/Input';
 import BookedItem from './BookedItem';
 import Modal from '@/components/modal/Modal';
 import EditBooking from './EditBooking';
+import NoBookings from './NoBookings';
+import UserActions from './UserActions';
 import User from '@/models/User';
 import { FormData } from '@/models/FormData';
 import css from './UserPortal.module.css';
-import NoBookings from './NoBookings'; // I am also not needed post testing
-import { useState } from 'react';
+import styles from './UserActions.module.css'; // the one button here has same styles as UserActions which have their own module
 
 export default function UserPortal(user: User) {
   const { id, bookings, ...userDetails } = user;
   const { item } = useSelector((state: RootState) => state.modal);
   const validate = useValidate({ update: { userId: id } });
-  const dispatch = useDispatch();
-  const [confirmDialog, setConfirmDialog] = useState(false);
-
-  function confirmHandler() {
-    setConfirmDialog((prev) => !prev);
-  }
-
-  function handleLogout() {
-    window.scrollTo(0, 125);
-    dispatch(logout());
-    dispatch(clearForm());
-  }
 
   return (
     <motion.section
@@ -42,10 +29,14 @@ export default function UserPortal(user: User) {
         <EditBooking id={id} {...item} />
       </Modal>
 
-      <motion.section className={css.portal}>
-        <div className={css.col}>
+      <section className={css.portal}>
+        <div className={css.user}>
           <h2>Details</h2>
-          <List className={css.user} items={Object.entries(userDetails)} keyFn={([key]) => key}>
+          <List
+            className={css['user-inputs']}
+            items={Object.entries(userDetails)}
+            keyFn={([key]) => key}
+          >
             {([key, value]) => (
               <p>
                 <span>{key}</span>
@@ -53,7 +44,7 @@ export default function UserPortal(user: User) {
               </p>
             )}
           </List>
-          <button onClick={validate}>SAVE</button>
+          <button className={styles.button} onClick={validate}>SAVE</button>
         </div>
 
         {bookings.length === 0 && <NoBookings />}
@@ -61,29 +52,9 @@ export default function UserPortal(user: User) {
         <List className={css.bookings} items={bookings} keyFn={({ id }) => id}>
           {(booking) => <BookedItem {...booking} />}
         </List>
-      </motion.section>
+      </section>
 
-      <div className={css.actions}>
-        <button onClick={handleLogout}>LOGOUT</button>
-        <AnimatePresence mode='wait'>
-          <motion.div
-            key={'a' + confirmDialog}
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 1 }}
-            exit={{ opacity: 0, x: 100 }}
-            className={css.dialog}
-          >
-            {!confirmDialog ? (
-              <button onClick={confirmHandler}>DELETE ACCOUNT</button>
-            ) : (
-              <>
-                <button onClick={() => dispatch(deleteUser(user.id))}>CONFIRM</button>
-                <button onClick={confirmHandler} className={css.cancel}>CANCEL</button>
-              </>
-            )}
-          </motion.div>
-        </AnimatePresence>
-      </div>
+      <UserActions id={id} />
     </motion.section>
   );
 }
