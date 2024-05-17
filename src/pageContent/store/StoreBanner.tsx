9dadcd9e56feb2335bec359e06d67afb08a31d9c
victor loginov <tkoe88@zoho.com>
2024-05-17 23:28:32 +0200

    save progress, can't get page transition to work

--- src/pageContent/store/StoreBanner.tsx
+++ src/pageContent/store/StoreBanner.tsx
@@ -14,33 +14,58 @@ interface Classes {
 
 export default function StoreBanner() {
   const classes: Classes = { 0: 'left', 1: 'centre', 2: 'right' };
-  // const x: Classes = { 0: '', 1: '', 2: '' }
   const images = [banner1, banner2, banner3];
   const leftText = ['Break the rules', 'Everybody be cool', 'The early bird'];
   const rightText = ['Ignore the machine', 'You be cool', 'captures the world'];
-  const [index, setIndex] = useState(1);
+  const [state, setState] = useState({ index: 1, direction: 1 });
 
-  function clickHandler(index: number) {
-    setIndex((prevIndex) => Math.max(0, Math.min(prevIndex + index, 2)));
+  function clickHandler(direction: number) {
+    setState((prevState) => ({
+      index: Math.max(0, Math.min(prevState.index + direction, 2)),
+      direction,
+    }));
   }
+
+  let initial, exit;
+
+  if (state.direction === 1) {
+    if (state.index === 0) {
+      [initial, exit] = ['100%', '-100%'];
+    } else if (state.index === 1) {
+      [initial, exit] = ['100%', '-100%'];
+    } else if (state.index === 2) {
+      [initial, exit] = ['-100%', '100%'];
+    }
+  } else if (state.direction === -1) {
+    if (state.index === 0) {
+      [initial, exit] = ['100%', '-100%'];
+    } else if (state.index === 1) {
+      [initial, exit] = ['-100%', '100%'];
+    } else if (state.index === 2) {
+      [initial, exit] = ['100%', '100%'];
+    }
+  }
+
+  console.log('initial', initial, '\n', 'exit', exit, '\n', state);
 
   return (
     <div className={css.banner}>
       <button onClick={() => clickHandler(-1)}>
         <FontAwesomeIcon icon={['fas', 'chevron-left']} />
       </button>
-      <AnimatePresence mode='popLayout'>
+      <AnimatePresence mode='wait' initial={false}>
         <motion.div
-          key={index}
-          className={`${css.card} ${css[classes[index as keyof Classes]]}`}
-          initial={{ x: '100%' }}
-          animate={{ x: 0 }}
-          exit={{ x: '-100%' }}
-          transition={{ type: 'tween', ease: 'linear', duration: 0.5 }}
+          // key={`${state.index}-${state.direction}`}
+          key={state.index}
+          className={`${css.card} ${css[classes[state.index as keyof Classes]]}`}
+          initial={{ opacity: 0, x: initial }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: exit }}
+          transition={{ type: 'tween', ease: 'linear', duration: 1 }}
         >
-          <p>{leftText[index]}</p>
-          <img src={images[index]} alt='models' />
-          <p>{rightText[index]}</p>
+          <p>{leftText[state.index]}</p>
+          <img src={images[state.index]} alt='models' />
+          <p>{rightText[state.index]}</p>
         </motion.div>
       </AnimatePresence>
       <button onClick={() => clickHandler(1)}>
