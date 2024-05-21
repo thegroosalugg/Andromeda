edbec8573fddb7553508f6a1959767a1844c541e
victor loginov <tkoe88@zoho.com>
2024-05-21 20:14:28 +0200

    created store item expandable section

--- src/pageContent/store/list/StoreItem.tsx
+++ src/pageContent/store/list/StoreItem.tsx
@@ -1,24 +1,41 @@
 import Clothes from '@/models/Clothes';
 import css from './StoreItem.module.css';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
-export default function StoreItem(item: Clothes) {
-  const { name, brand, image, price } = item;
-  const [isActive, setIsActive] = useState(false);
+interface StoreProps {
+  item: Clothes;
+  clickHandler: () => void;
+  activeId: string | undefined;
+}
 
-  function clickHandler() {
-    setIsActive(prevState => !prevState);
-  }
+export default function StoreItem({ item, clickHandler, activeId }: StoreProps) {
+  const { id, name, brand, image, price } = item;
+  const isActive = activeId === id;
+  console.log('RENDERING ID', id);
 
   return (
-    <motion.article className={css.item} initial={{ scale: 0.5 }} animate={{ scale: isActive ? 2 : 1 }}>
+    <motion.article
+      className={css.item}
+      initial={{ scale: 0.5 }}
+      animate={{ scale: 1, height: 'auto' }}
+    >
       <img src={image} alt='clothes' onClick={clickHandler} />
       <div className={css.details}>
         <h4>{brand}</h4>
         <h5>{name}</h5>
         <h3>${price}</h3>
       </div>
+      <AnimatePresence mode='sync'>
+        {isActive && (
+          <motion.div
+            layoutId='store-item'
+            initial={{ height: 0 }}
+            animate={{ height: '100px' }}
+            exit={{ height: 0, transition: { delay: 0.5 } }}
+            transition={{ duration: 0.5, type: 'easeInOut' }}
+          />
+        )}
+      </AnimatePresence>
     </motion.article>
   );
 }
--- src/pageContent/store/list/StoreList.tsx
+++ src/pageContent/store/list/StoreList.tsx
@@ -3,13 +3,17 @@ import { RootState } from '@/store/types';
 import List from '@/components/list/List';
 import css from './StoreList.module.css';
 import StoreItem from './StoreItem';
+import { useState } from 'react';
 
 export default function StoreList() {
   const { clothes } = useSelector((state: RootState) => state.items);
+  const [activeId, setActiveId] = useState<string | undefined>();
 
   return (
     <List className={css.grid} items={clothes} keyFn={({ id }) => id}>
-      {(item) => <StoreItem {...item} />}
+      {(item) => (
+        <StoreItem item={item} clickHandler={() => setActiveId(item.id)} activeId={activeId} />
+      )}
     </List>
   );
 }
