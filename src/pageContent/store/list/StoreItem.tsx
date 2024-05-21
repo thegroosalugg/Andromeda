34ec8bfbf8bbe0bd616eabb5ee8aab5cc45d6e07
victor loginov <tkoe88@zoho.com>
2024-05-21 22:13:03 +0200

    made store items expandable

--- src/pageContent/store/list/StoreItem.tsx
+++ src/pageContent/store/list/StoreItem.tsx
@@ -14,28 +14,47 @@ export default function StoreItem({ item, clickHandler, activeId }: StoreProps) 
   console.log('RENDERING ID', id);
 
   return (
-    <motion.article
-      className={css.item}
-      initial={{ scale: 0.5 }}
-      animate={{ scale: 1, height: 'auto' }}
+    <motion.li
+      className={`${css.item} ${isActive ? css.expanded : ''}`}
+      // className={css.item}
+      // style={isActive ? { gridColumn: '1 / -1' } : {}}
+      variants={{
+        hidden: { scale: 0.5, opacity: 0, width: 0 },
+        visible: { scale: 1, opacity: 1, width: 'auto' },
+      }}
     >
-      <img src={image} alt='clothes' onClick={clickHandler} />
-      <div className={css.details}>
-        <h4>{brand}</h4>
-        <h5>{name}</h5>
-        <h3>${price}</h3>
-      </div>
-      <AnimatePresence mode='sync'>
+      <section>
+        <img src={image} alt='clothes' onClick={clickHandler} />
+        <div className={css.details}>
+          <h4>{brand}</h4>
+          <h5>{name}</h5>
+          <h3>${price}</h3>
+        </div>
+        <AnimatePresence mode='popLayout'>
+          {isActive && (
+            <motion.div
+              // layoutId='store-item'
+              initial={{ height: 0 }}
+              animate={{ height: '100px' }}
+              exit={{ height: 0, transition: { delay: 0.5 } }}
+              transition={{ duration: 0.5, type: 'easeInOut' }}
+            />
+          )}
+        </AnimatePresence>
+      </section>
+      <AnimatePresence mode='popLayout'>
         {isActive && (
           <motion.div
-            layoutId='store-item'
-            initial={{ height: 0 }}
-            animate={{ height: '100px' }}
-            exit={{ height: 0, transition: { delay: 0.5 } }}
-            transition={{ duration: 0.5, type: 'easeInOut' }}
-          />
+            // layoutId='store-item'
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: 'auto', opacity: 1 }}
+            exit={{ width: 0, opacity: 0, transition: { delay: 0.5 } }}
+            transition={{ duration: 1, type: 'easeInOut' }}
+          >
+            <p>HELLO</p>
+          </motion.div>
         )}
       </AnimatePresence>
-    </motion.article>
+    </motion.li>
   );
 }
--- src/pageContent/store/list/StoreList.tsx
+++ src/pageContent/store/list/StoreList.tsx
@@ -1,19 +1,29 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/types';
-import List from '@/components/list/List';
 import css from './StoreList.module.css';
 import StoreItem from './StoreItem';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 export default function StoreList() {
   const { clothes } = useSelector((state: RootState) => state.items);
   const [activeId, setActiveId] = useState<string | undefined>();
 
   return (
-    <List className={css.grid} items={clothes} keyFn={({ id }) => id}>
-      {(item) => (
-        <StoreItem item={item} clickHandler={() => setActiveId(item.id)} activeId={activeId} />
-      )}
-    </List>
+    <motion.ul
+      className={css.grid}
+      initial='hidden'
+      animate='visible'
+      transition={{ staggerChildren: 0.05 }}
+    >
+      {clothes.map((item) => (
+        <StoreItem
+          key={item.id}
+          item={item}
+          clickHandler={() => setActiveId(item.id)}
+          activeId={activeId}
+        />
+      ))}
+    </motion.ul>
   );
 }
