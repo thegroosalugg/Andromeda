89f7fff2e8fc5ed41a7b1e585375078974fdcd37
victor loginov <tkoe88@zoho.com>
2024-05-26 16:08:14 +0200

    added header exit animations

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import css from './Header.module.css';
 import useUIConfig from '@/hooks/useUIConfig';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -19,18 +19,28 @@ export default function Header() {
       }}
       transition={{ ease: 'easeInOut', duration: 0.8 }}
     >
-      <motion.h1
-        key={pathname}
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: [0, 0, 0, 0.5, 1], x: pathname === '/store' ? 50 : 0 }}
-      >
-        {text && text}
-      </motion.h1>
-      {pathname === '/store' && (
-        <button>
-          <FontAwesomeIcon icon={['fas', 'cart-shopping']} size='2x' />
-        </button>
-      )}
+      <AnimatePresence mode='popLayout'>
+        <motion.h1
+          key={pathname}
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: [0, 0, 0, 0.5, 1], x: pathname === '/store' ? 50 : 0 }}
+          exit={{ scaleY: 0, opacity: 0 }}
+        >
+          {text && text}
+        </motion.h1>
+        {pathname === '/store' && (
+          <motion.button
+            initial={{ y: -100, x: -20, rotate: 540 }}
+            animate={{ y: [25, 0], x: 0, rotate: 0 }}
+            exit={{ y: 100, rotate: 540, opacity: 0 }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9, rotate: -400 }}
+            transition={{ type: 'spring', stiffness: 50, damping: 5 }}
+          >
+            <FontAwesomeIcon icon={['fas', 'cart-shopping']} size='2x' />
+          </motion.button>
+        )}
+      </AnimatePresence>
     </motion.header>
   );
 }
