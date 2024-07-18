24b0a4c8ac35f730d898262e28de61ccf7f78519
victor loginov <tkoe88@zoho.com>
2024-07-18 23:42:54 +0200

    cascading asteroid entry animations added

--- src/pageContent/explore/Asteroids.tsx
+++ src/pageContent/explore/Asteroids.tsx
@@ -7,27 +7,38 @@ import rand from '@/util/rand';
 export default function Asteroids() {
   const { width } = useScreen();
   const flexRow = width > 440;
-  const multiplier = flexRow ? 0.01 : 0.04;
+  const multiplier = flexRow ? 0.015 : 0.04;
 
   const numRows = Math.ceil(width * multiplier);
 
   return (
-    <motion.div className={css['asteroids']}>
+    <motion.div
+      initial='hidden'
+      animate='visible'
+      transition={{ staggerChildren: 0.05 }}
+      className={css['asteroids']}
+    >
       {Array.from({ length: numRows }).map((_, rowIndex) => (
-        <motion.div key={rowIndex}>
+        <motion.div
+          key={rowIndex}
+          variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 }}}
+          transition={{ staggerChildren: 0.05 }}
+        >
           {Array.from({ length: rand(2, 7) }).map((_, asteroidIndex) => (
             <motion.img
               key={asteroidIndex}
               src={asteroid}
               alt='Asteroid'
-              initial={{ opacity: 0, scale: 0, rotate: 0, x: 0, y: 0 }}
-              animate={{
-                opacity: 1,
-                filter: `brightness(${rand(4, 8) * 0.1}) sepia(1) hue-rotate(5deg)`,
-                scale: rand(4, 10) * 0.1,
-                rotate: rand(-360, 360),
-                x: rand(-10, 10),
-                y: rand(-10, 10),
+              variants={{
+                hidden: { opacity: 0, scale: 0, rotate: 0, x: 0, y: 0 },
+                visible: {
+                  opacity: 1,
+                  filter: `brightness(${rand(4, 8) * 0.1}) sepia(1) hue-rotate(5deg)`,
+                  scale: rand(4, 10) * 0.1,
+                  rotate: rand(-360, 360),
+                  x: rand(-10, 10),
+                  y: rand(-10, 10),
+                },
               }}
             />
           ))}
