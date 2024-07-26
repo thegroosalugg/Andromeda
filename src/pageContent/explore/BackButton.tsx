1bf55521d2d190418119af3e3de7a14de69db98c
victor loginov <tkoe88@zoho.com>
2024-07-27 00:13:04 +0200

    minor button update

--- src/pageContent/explore/BackButton.tsx
+++ src/pageContent/explore/BackButton.tsx
@@ -10,9 +10,10 @@ export default function BackButton() {
     <motion.button
       className={css.button}
       initial={{ scaleY: 0, opacity: 0, height: 0 }}
-      animate={{ scaleY: 1, opacity: 1, height: 'auto' }}
+      animate={{ scaleY: 1, opacity: 1, height: 'auto', background: '#ff105f' }}
          exit={{ scaleY: 0, opacity: 0, height: 0 }}
       transition={{ duration: 0.5 }}
+      whileHover={{ background: '#ffad06', color: '#535151' }}
       onClick={() => activeHandler('all')}
     >
       Fly Back
