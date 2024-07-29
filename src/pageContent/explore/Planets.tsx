fac4f1717a6f82e6dbc8c8be0bc94ff3d805981a
victor loginov <tkoe88@zoho.com>
2024-07-29 10:37:58 +0200

    updated planets component further with several changes

--- src/pageContent/explore/Planets.tsx
+++ src/pageContent/explore/Planets.tsx
@@ -12,18 +12,23 @@ import uranus from '@/assets/planets/uranus.png';
 import neptune from '@/assets/planets/neptune.png';
 import css from './Planets.module.css';
 
-// prettier-ignore
-const config = (planet: string, width: number, height: number, isActive: string) => {
-  const name = planet.match(/(mercury|venus|earth|mars|jupiter|saturn|uranus|neptune)/)![0] as keyof typeof props;
+export default function Planets({ outer }: { outer?: boolean }) {
+  const planets = outer ? [jupiter, saturn, uranus, neptune] : [mercury, venus, earth, mars];
+  const { isActive, activeHandler } = useContext(ExploreContext);
+  const { width, height } = useScreen();
+  const activeClass = (isActive === 'inner' && !outer) || (isActive === 'outer' && outer);
+  const mobileDev = (width >= 320 && width <= 440) || (width / height > 1.8 && width < 1024);
 
-  const adjust = (base: number) =>
-    base * // 1: targets mobiles, 2: landscape mobiles via aspect ratio
-    ((width >= 320 && width <= 440) || (width / height > 1.8 && width < 1024) ? 0.5 : 1) *
-    (isActive === 'inner' ? 3 : isActive === 'outer' ? 2 : 1);
+  const nameOf = (planet: string) =>
+    planet.match(
+      /(mercury|venus|earth|mars|jupiter|saturn|uranus|neptune)/
+    )![0] as keyof typeof config;
 
+  const adjust = (base: number) => base * (mobileDev ? 0.5 : 1) * (activeClass ? 2 : 1);
   const margin = width > height ? 'marginBottom' : 'marginLeft';
 
-  const props = {
+  // prettier-ignore
+  const config = {
     mercury: { width: adjust( 20), [margin]: 20, rotate: 0 },
     venus:   { width: adjust( 30), [margin]: 30, rotate: 0 },
     earth:   { width: adjust( 40), [margin]: 40, rotate: 0 },
@@ -34,54 +39,48 @@ const config = (planet: string, width: number, height: number, isActive: string)
     neptune: { width: adjust( 70), [margin]: 20, rotate: 0 },
   };
 
-  return { ...props[name] };
-};
-
-export default function Planets({ outer }: { outer?: boolean }) {
-  const planets = outer ? [jupiter, saturn, uranus, neptune] : [mercury, venus, earth, mars];
-  const { isActive, activeHandler } = useContext(ExploreContext);
-  const { width, height } = useScreen();
-  console.log(width, height); // logData
-  // width <= 440 && outer && isActive !== 'outer' && planets.reverse();
-  const activeClass = (isActive === 'inner' && !outer) || (isActive === 'outer' && outer);
-
   return (
     <motion.section
       className={`${css['planets']} ${activeClass ? css['active'] : ''}`}
       onClick={() => activeHandler(outer ? 'outer' : 'inner')}
       initial='hidden'
       animate='visible'
-      exit={{ x: outer ? 500 : -500, opacity: 0, transition: { duration: 0.8 } }}
+      exit={{ scale: 0, opacity: 0, transition: { duration: 0.8 } }}
       transition={{ staggerChildren: 0.2, delayChildren: outer ? 1 : 0.5 }}
+      // prettier-ignore
       style={{
         flexDirection: width > height ?       'row' : 'column',
               padding: width > height ? '2rem 1rem' : '1rem 0',
                   gap: width > height ?    '0.5rem' : '1rem',
       }}
     >
-      {planets.map((planet) => (
-        <motion.div
-          key={planet}
-          layout
-          transition={{ duration: 1.2 }} // controls layout transition
-          variants={{
-            hidden: { opacity: 0, scale: 0 },
-            visible: {
-              opacity: [0, 1],
-              scale: 1,
-              transition: { type: 'tween', ease: 'linear', duration: 0.5 },
-            },
-          }}
-        >
-          <motion.img
-            src={planet}
-            alt={planet}
-            initial={config(planet, width, height, isActive)}
-            animate={config(planet, width, height, isActive)}
-            transition={{ duration: 0.5 }}
-          />
-        </motion.div>
-      ))}
+      {planets.map((planet) => {
+        const key = nameOf(planet);
+
+        return (
+          <motion.div
+            key={planet}
+            layout
+            transition={{ duration: 1.2 }} // controls layout transition
+            variants={{
+              hidden: { opacity: 0, scale: 0 },
+              visible: {
+                opacity: [0, 1],
+                scale: 1,
+                transition: { type: 'tween', ease: 'linear', duration: 0.5 },
+              },
+            }}
+          >
+            <motion.img
+              src={planet}
+              alt={key}
+              initial={{ ...config[key] }}
+              animate={{ ...config[key] }}
+              transition={{ duration: 1 }}
+            />
+          </motion.div>
+        );
+      })}
     </motion.section>
   );
 }
