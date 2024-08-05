82d62cdfc1af53cb0d35acc78f8ef5c2bdc06265
victor loginov <tkoe88@zoho.com>
2024-08-05 15:13:41 +0200

    back button updated in space components

--- src/pageContent/explore/BackButton.tsx
+++ src/pageContent/explore/BackButton.tsx
@@ -1,22 +1,55 @@
 import { motion } from 'framer-motion';
 import { useContext } from 'react';
 import { ExploreContext } from '@/pages/explore/ExploreContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FlipProp, IconProp } from '@fortawesome/fontawesome-svg-core';
 import css from './BackButton.module.css';
 
+// prettier-ignore
+const texts = {
+    sun: 'The Sun',
+  inner: 'Inner Solar System',
+  outer: 'Outer Solar System',
+    ast: 'Asteroid Belt',
+};
+
+// prettier-ignore
+const icons: { [key: string]: IconProp } = {
+    all: 'sun', // only used to remove console errors
+    sun: 'sun',
+  inner: 'satellite-dish',
+  outer: 'user-astronaut',
+    ast: 'meteor',
+};
+
 export default function BackButton() {
-  const { activeHandler } = useContext(ExploreContext);
+  const { activeFC, activeHandler } = useContext(ExploreContext);
 
   return (
     <motion.button
       className={css.button}
       initial={{ scaleY: 0, opacity: 0, height: 0 }}
       animate={{ scaleY: 1, opacity: 1, height: 'auto', background: '#ff105f' }}
-         exit={{ scaleY: 0, opacity: 0, height: 0 }}
+      exit={{ scaleY: 0, opacity: 0, height: 0 }}
       transition={{ duration: 0.5 }}
       whileHover={{ background: '#ffad06', color: '#535151' }}
       onClick={() => activeHandler('all')}
     >
-      Fly Back
+      <span>
+        <FontAwesomeIcon
+          icon={icons[activeFC]}
+          size='xs'
+          beat={activeFC === 'sun'}
+          fade={activeFC === 'inner'}
+          bounce={activeFC === 'ast'}
+        />
+        {' ' + texts[activeFC as keyof typeof texts]}
+      </span>
+      <span>
+        {'Back '}
+        {/* flip prop is a valid animation. TypeScript is wrong. */}
+        <FontAwesomeIcon icon='rocket' flip={true as unknown as FlipProp} />
+      </span>
     </motion.button>
   );
 }
